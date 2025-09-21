7b4432d874eee2a459b3592ed53db0825d86fae1
Bryan Blankenburg <bryan.blankenburg@outlook.com>
2025-09-20 23:26:18 -0700

    fix: remove submodule and add server as regular folder

---
+++ server/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true, // For dev only! Don't use in production
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
